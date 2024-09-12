cf66549d36e0aef18b6caeef329d2c8efc11599f
Ana Gabrielle <127758805+Aninha2908@users.noreply.github.com>
2024-09-12 10:03:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const CaixaPrincipal = document.querySelector(". caixa-principal");
+const CaixaPerguntas = document.querySelector(". caixa-perguntas");
+const CaixaAlternativas = document.querySelector(". caixa-alternativas");
+const CaixaResultado = document.querySelector(". caixa-resultado");
+const CaixaResultado = document.querySelector(". caixa-resultado");
+
+const perguntas = [
+    { 
+        enunciado: "pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+
+    },
+    {
+        enunciado: "pergunta 3",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "pergunta 4",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+    enunciado: "pergunta 5",
+    alternativas: [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+
+    },
+
+]
